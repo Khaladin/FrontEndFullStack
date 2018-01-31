31d08855481d37c5a4a8119b625abcf556c54302
Cory Hoban <hobancor@gmail.com>
2018-01-31 11:21:07 -0500

    create form fields

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
 
 class Signin extends Component {
+  handleFormSubmit({email, password}) {
+    console.log(email, password);
+    //Need to do something to log user in
+  }
+
   render() {
+    const renderInput = field => (
+      <div>
+        <input {...field.input} type={field.type} className="form-control" />
+        {field.meta.touched && field.meta.error}
+        <span>{field.meta.error}</span>
+      </div>
+    )
+
+    const { handleSubmit, fields: {email, password}} = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email:</label>
-          <input className="form-control" />
+          <Field
+            name="email"
+            component={renderInput}
+            type="text" />
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input className="form-control" />
+          <Field
+            name="password"
+            component={renderInput}
+            type="text" />
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form>
